fedef12067a372d0fd1186d6e6d65950588f37ca
Vu1911 <71309497+Vu1911@users.noreply.github.com>
2021-05-08 00:20:38 +0700

    Update FlightService.java

--- src/main/java/com/se2/hanuairline/service/FlightService.java
+++ src/main/java/com/se2/hanuairline/service/FlightService.java
@@ -162,9 +162,6 @@ public class FlightService {
         Optional<Gate> arrival_gateData = gateRepository.findById(request.getArrival_gate_id());
         Optional<Gate> departure_gateData = gateRepository.findById(request.getDeparture_gate_id());
 
-        DiscountEvent discountEvent = discountEventService.getById(request.getDiscount_id());
-        System.out.println("DISCOUNT : " +discountEvent);
-
 
         // check duplication
         Optional<Flight> check = flightRepository.findByArrivalTimeAndDepartureTimeAndArrivalGate_IdAndDepartureGate_Id(request.getArrival_time(), request.getDeparture_time(), request.getArrival_gate_id(), request.getDeparture_gate_id());
